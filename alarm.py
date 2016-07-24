56833e5bd769583f477adce6c6bf8909f685b48e
Kenny Vu <Kenny Vu>
2016-07-24 19:39:32 +0100

    Changelog: Sound is now played when the timer duration ends Note: Winsounds should work for windows only

--- alarm.py
+++ alarm.py
@@ -1,7 +1,11 @@
 import time
+import winsound
 import sys
+
+
 
 duration = int(input("how long do you want to sleep for?"));
 
 time.sleep(duration);
 print("WAKE UP");
+winsound.PlaySound('sound.wav', winsound.SND_FILENAME)
